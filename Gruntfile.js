c1ea893b933b7ede11ebdea429d3fb600dcc0fdd
swistach <swistach@gmail.com>
2018-06-07 16:09:57 +0200

    Added: notification about the latest version of a library.

--- Gruntfile.js
+++ Gruntfile.js
@@ -217,7 +217,7 @@ module.exports = function (grunt) {
 
     gitHelper.getDocsVersions().then(function(branches) {
       branches = branches.filter(function(branch) {
-        return /^\d+\.\d+\.\d+$/.test(branch) && !/^draft\-/.test(branch);
+        return /^\d+\.\d+\.\d+$/.test(branch) && !/^draft\-/.test(branch) && semver.gte(branch, '0.32.0');
       });
 
       var content = 'docVersions && docVersions(' + JSON.stringify(branches.reverse()) + ')';
