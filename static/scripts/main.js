d97d6c8d25d44c55a856335e5cdaac012a2e67ce
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2018-08-09 11:25:44 +0200

    fixed search id conflict

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,8 +2,8 @@ $(function () {
   jQuery.fx.off = true;
 
   // Search Items
-  $('#search').on('keyup', onSearchKeyUp);
-  document.querySelector('#search').focus();
+  $('#search-form').on('keyup', onSearchKeyUp);
+  document.querySelector('#search-form').focus();
 
   // Toggle when click an item element
   $('.navigation').on('click', '.title', function (event) {
